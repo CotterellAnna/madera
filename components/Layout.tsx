54d2aa6d5e5a53fa3dfcd3dded63c408ceee0988
Obalari Cotterell <cotterellanna@gmail.com>
2025-11-15 13:36:54 +0100

    mobile layout (navbar)

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import { AppShell, Group, Burger, UnstyledButton, Image, BackgroundImage, Container, Flex, ActionIcon } from "@mantine/core";
+import { AppShell, Group, Burger, UnstyledButton, Image, BackgroundImage, Container, Flex, ActionIcon, Stack, Button } from "@mantine/core";
 import { useDisclosure, useWindowScroll } from "@mantine/hooks";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -15,7 +15,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 backgroundColor:
                     scroll.y > 50 ? "var(--mantine-color-madera-9)" : "transparent",
             }}>
-                <Group h="100%" px="md">
+                <Group h="98%" px="md">
                     <Group justify="space-between" flex={1}>
                         <Group gap={0}>
                             <ActionIcon variant="transparent" onClick={toggle}>
@@ -23,7 +23,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                             </ActionIcon>
                         </Group>
                         <Image w={100} src={"/images/logo.svg"} alt="madera" />
-                        <Flex ml="xl" gap={"lg"} w={"auto"}>
+                        <Flex ml="xl" gap={"lg"} w={"auto"} visibleFrom="md">
                             <ActionIcon variant="transparent">
                                 <Image src={"/images/icons/search.svg"} alt="search" />
                             </ActionIcon>
@@ -38,11 +38,18 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 </Group>
             </AppShell.Header>
 
-            <AppShell.Navbar py="md" px={4}>
-                <Image src={"/images/icons/search.svg"} alt="search" />
-                <UnstyledButton>Blog</UnstyledButton>
-                <UnstyledButton>Contacts</UnstyledButton>
-                <UnstyledButton>Support</UnstyledButton>
+            <AppShell.Navbar py="md" px={4} bg={"madera.9"}>
+                <Stack ml="xl" gap={"lg"} w={"auto"}>
+                    <Button justify="flex-start" c={"madera.1"} variant="transparent" leftSection={<Image src={"/images/icons/search.svg"} alt="search" />}>
+                        Search Madera
+                    </Button>
+                    <Button justify="flex-start"  c={"madera.1"} variant="transparent" leftSection={<Image src={"/images/icons/shopping_cart.svg"} alt="search" />}>
+                        Madera Cart
+                    </Button>
+                    <Button justify="flex-start"  c={"madera.1"} variant="transparent" leftSection={<Image src={"/images/icons/user.svg"} alt="search" />}>
+                        Profile
+                    </Button>
+                </Stack>
             </AppShell.Navbar>
 
             <AppShell.Main px={{ base: "30px", md: "40px" }}>
